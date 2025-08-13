1af181a17eea0531aa2cee2c0d1fcde43603559f
Tomer2006 <tomerprice2008@gmail.com>
2025-08-13 17:37:56 +0300

    fix circle sizing

--- modules/layout.js
+++ modules/layout.js
@@ -1,11 +1,17 @@
 import { W, H } from './canvas.js';
+import { settings } from './constants.js';
 
 const pack = d3.pack().padding(2);
 
 export function layoutFor(subtree) {
+  const exp = typeof settings.valueExponent === 'number' ? settings.valueExponent : 1;
   const h = d3
     .hierarchy(subtree)
     .sum(d => (d.children && d.children.length ? 0 : 1))
+    .eachAfter(n => {
+      // Apply damping exponent to soften extreme size differences
+      if (n.value && exp !== 1) n.value = Math.pow(n.value, exp);
+    })
     .sort((a, b) => b.value - a.value);
   const diameter = Math.min(W, H) - 40;
   pack.size([diameter, diameter])(h);
